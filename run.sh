1b92228f0e652b374d1172a71292c6d4f56bd3e7
Peter Kokot <peterkokot@gmail.com>
2017-02-12 15:47:13 +0100

    Refactor servers and apps

--- run.sh
+++ run.sh
@@ -1,102 +1,104 @@
 #!/bin/bash
 
+# Benchmarking helper script
+
 # Output usage documentation and help
 function usage () {
 cat <<END
 Custom helper commands for managing the benchmarks
-Usage: run.sh COMMAND [arg...]
-Commands:'
+Usage: run.sh COMMAND APP [arg...]
+Commands:
     init              Install applications
-    apache            Runs Apache static file benchmark
-    nginx             Runs Nginx static file benchmark
-    apache-php        Runs Apache with PHP-CGI benchmark
-    litespeed-php     Runs OpenLiteSpeed with LSAPI benchmark
-    litespeed-swoole  Runs OpenLiteSpeed with Swoole (TCP/IP) benchmark
-    nginx-php         Runs Nginx with PHP-FPM (TCP/IP Socket) benchmark
-    nginx-php-uds     Runs Nginx with PHP-FPM (Unix Domain Socket) benchmark
-    swoole            Runs Swoole benchmark
-    nginx-swoole      Runs Nginx with Swoole benchmark
-    nginx-swoole-uds  Runs Nginx with Swoole benchmark
+    apache            Run Apache static file benchmark
+    apache-php        Run Apache with PHP-CGI benchmark
+    nginx             Run Nginx static file benchmark
+    nginx-fpm         Run Nginx with PHP-FPM (TCP/IP Socket) benchmark
+    nginx-fpm-uds     Run Nginx with PHP-FPM (Unix Domain Socket) benchmark
+    litespeed         Run OpenLiteSpeed static file benchmark
+    litespeed-php     Run OpenLiteSpeed with LSAPI benchmark
+    litespeed-swoole  Run OpenLiteSpeed with Swoole (TCP/IP) benchmark
+    swoole            Run Swoole benchmark
+    nginx-swoole      Run Nginx with Swoole (TCP/IP) benchmark
+    nginx-swoole-uds  Run Nginx with Swoole (Unix Domain Socket) benchmark
+    node              Run Node.js and Express benchmark Hello world benchmark
+Applications:
+    html              Static HTML file
+    php               Hello world PHP application
+    symfony           Symfony application
+    laravel           Laravel application
+    swoole            Hello world PHP Swoole application
 END
+}
+
+# Parse YAML configuration files
+function parse_yaml {
+    local prefix=$2
+    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+    sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+    awk -F$fs '{
+        indent = length($1)/2;
+        vname[indent] = $2;
+        for (i in vname) {if (i > indent) {delete vname[i]}}
+        if (length($3) > 0) {
+            vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+            printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+        }
+    }'
+}
+
+# Set configuration values
+eval $(parse_yaml config.yml)
+
+# Generic benchmark
+# Arguments
+#   $1 - setup name
+#   $2 - application name
+function run() {
+    sum=0
+    for ((i=1; i<=$benchmarks; i++)); do
+        docker-compose -f servers/$1/docker-compose.yml -f servers/$1/docker-compose.$2.yml up -d --force-recreate
+        echo "Benchmarking..."
+        sleep $sleep
+
+        ab -c $concurency -n $requests -k http://localhost/ > results/$1-$2_$i.txt
+        rqs=$(grep -o -P '(?<=Requests per second:    ).*(?= \[)' results/$1-$2_$i.txt)
+
+        docker-compose -f servers/$1/docker-compose.yml -f servers/$1/docker-compose.$2.yml down
+        sum=$(echo "$sum + $rqs" | bc)
+    done
+    average=$(echo "$sum / $benchmarks" | bc)
+    echo $1" "$2" "$average >> $results
 }
 
 # If arguments are passed, for example ./run.sh nginx
 if [ $# -gt 0 ];then
 
+    stacks=("apache" "apache-php" "nginx" "nginx-fpm" "nginx-swoole" "litespeed" "litespeed-php" "litespeed-swoole" "swoole" "php-pm" "node" "nginx-swoole-uds" "nginx-fpm-uds")
+
     if [ "$1" == "init" ]; then
-        shift 1
+        # Pull Docker images
+        for i in "${stacks[@]}"
+        do
+            docker-compose -f servers/$i/docker-compose.yml pull
+        done
+
+        # Install Express node.js
+        docker-compose -f servers/node/docker-compose.yml run --rm app npm install
+
+        # Install Symfony
         cd apps/symfony
         composer install
         composer dump-autoload --optimize
+
+        # Install Laravel
         cd ../laravel
         composer install
         composer dump-autoload --optimize
 
-    elif [ "$1" == "apache" ]; then
-        shift 1
-        docker-compose -f servers/apache/docker-compose.yml up -d --build --force-recreate
-        ab -c 200 -n 200000 -k -r http://localhost/
-        docker-compose -f servers/apache/docker-compose.yml down
-
-    elif [ "$1" == "nginx" ]; then
-        shift 1
-        docker-compose -f servers/nginx/docker-compose.yml up -d --build --force-recreate
-        ab -c 200 -n 200000 -k -r http://localhost/
-        docker-compose -f servers/nginx/docker-compose.yml down
-
-    elif [ "$1" == "litespeed" ]; then
-        shift 1
-        docker-compose -f servers/litespeed/docker-compose.yml up -d --build --force-recreate
-        ab -c 200 -n 200000 -k -r http://localhost/
-        docker-compose -f servers/litespeed/docker-compose.yml down
-
-    elif [ "$1" == "apache-php" ]; then
-        shift 1
-        docker-compose -f servers/apache-php/docker-compose.yml up -d --build --force-recreate
-        ab -c 200 -n 200000 -k -r http://localhost/
-        docker-compose -f servers/apache-php/docker-compose.yml down
-
-    elif [ "$1" == "litespeed-php" ]; then
-        shift 1
-        docker-compose -f servers/litespeed-php/docker-compose.yml up -d --build --force-recreate
-        ab -c 200 -n 200000 -k -r http://localhost/
-        docker-compose -f servers/litespeed-php/docker-compose.yml down
-
-    elif [ "$1" == "litespeed-swoole" ]; then
-        shift 1
-        docker-compose -f servers/litespeed-swoole/docker-compose.yml up -d --build --force-recreate
-        ab -c 200 -n 200000 -k -r http://localhost/
-        docker-compose -f servers/litespeed-swoole/docker-compose.yml down
-
-    elif [ "$1" == "nginx-php" ]; then
-        shift 1
-        docker-compose -f servers/nginx-php/docker-compose.yml up -d --build --force-recreate
-        ab -c 200 -n 200000 -k -r http://localhost/
-        docker-compose -f servers/nginx-php/docker-compose.yml down
-
-    elif [ "$1" == "nginx-php-uds" ]; then
-        shift 1
-        docker-compose -f servers/nginx-php-uds/docker-compose.yml up -d --build --force-recreate
-        ab -c 200 -n 200000 -k -r http://localhost:8080/
-        docker-compose -f servers/nginx-php-uds/docker-compose.yml down
-
-    elif [ "$1" == "swoole" ]; then
-        shift 1
-        docker-compose -f servers/swoole/docker-compose.yml up -d --build --force-recreate
-        ab -c 200 -n 200000 -k -r http://localhost/
-        docker-compose -f servers/swoole/docker-compose.yml down
-
-    elif [ "$1" == "nginx-swoole" ]; then
-        shift 1
-        docker-compose -f servers/nginx-swoole/docker-compose.yml up -d --build --force-recreate
-        ab -c 200 -n 200000 -k -r http://localhost/
-        docker-compose -f servers/nginx-swoole/docker-compose.yml down
-
-    elif [ "$1" == "nginx-swoole-uds" ]; then
-        shift 1
-        docker-compose -f servers/nginx-swoole-uds/docker-compose.yml up -d --build --force-recreate
-        ab -c 200 -n 200000 -k -r http://localhost/
-        docker-compose -f servers/nginx-swoole-uds/docker-compose.yml down
+    elif [[ "${stacks[@]}" =~ "$1" ]]; then
+        run $1 $2
 
     else
         usage
