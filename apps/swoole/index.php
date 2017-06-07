308347ae9713f3e71e974ca86543062a3f4dbdb1
Peter Kokot <peterkokot@gmail.com>
2017-06-07 19:50:12 +0200

    Update swoole PHP app

--- apps/swoole/index.php
+++ apps/swoole/index.php
@@ -1,8 +1,14 @@
+#!/usr/bin/env php
 <?php
 
 $http = new swoole_http_server("0.0.0.0", 9501);
 
 $http->addlistener("/run/php/swoole.sock", 0, SWOOLE_UNIX_STREAM);
+
+$http->set([
+    'user' => 'www-data',
+    'group' => 'www-data'
+]);
 
 $http->on('request', function ($request, $response) {
     $response->header("Content-Type", "text/html; charset=utf-8");
